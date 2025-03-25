3264ab66b6b73cfb945e4d81df9159939d73d1a0
Wxfee25 <131695866+Wxfee25@users.noreply.github.com>
2025-03-25 11:48:03 -0500

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,25 +1,30 @@
 @echo off
 title Install_Wxfee25
 color 0A
+
+:menu
+Cls
 echo =======================================================================================================================
-echo      						   Install_Wxfee25
+echo                        Install_Wxfee25
 echo =======================================================================================================================
 echo.
-echo 1)Informacion	
-echo y) Salir	
+echo 1) Datos_bloc_de_notas_Wxfee25
+echo y) Salir
 echo.
 set /p opcion="Elige una opcion: "
 
 if %opcion%==1 (
-    echo Abriendo Bloc de Notas...
-    start notepad
+    echo Creando archivo.txt con el contenido...
+    echo 1 > archivo.txt
+    echo Abriendo archivo.txt en el Bloc de Notas...
+    start notepad archivo.txt
     pause
     goto menu
 )
-if %opcion%==y (
+if /I "%opcion%"=="y" (
     echo Saliendo...
     exit
 )
-echo Opcion no encontrado, vuelve a intentar.
+echo Opcion no encontrada, vuelve a intentar.
 pause
 goto menu
