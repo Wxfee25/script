749c73cd92c035bb473592cd037885aa727ada03
Wxfee25 <131695866+Wxfee25@users.noreply.github.com>
2025-03-25 12:18:37 -0500

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -5,7 +5,7 @@ color 0A
 :menu
 Cls
 echo =======================================================================================================================
-echo                        Install_Wxfee25
+echo  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Install_Wxfee25 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 echo =======================================================================================================================
 echo.
 echo 1) Datos_bloc_de_notas_Wxfee25
@@ -13,14 +13,14 @@ echo y) Salir
 echo.
 set /p opcion="Elige una opcion: "
 
-if %opcion%==1 (
-    echo Creando archivo.txt con el contenido...
-echo  ejecuta Windows+R
-%temp% / temp / prefech /
-
-> Limpiar_basuras_winds_Iso.txt
-    echo Abriendo Limpiar_basuras_winds_Iso.txt Abriendo...
-    start notepad Limpiar_basuras_winds_Iso.txt
+if "%opcion%"=="1" (
+    echo Creando el archivo con el contenido deseado...
+    REM Aquí metes el contenido que quieras en el archivo
+    echo Ejecutar Windows +R
+%temp% / temp / prefech /> Datos_bloc_de_notas_Wxfee25.txt
+    
+    echo Abriendo bloc de notas...
+    start notepad Datos_bloc_de_notas_Wxfee25.txt
     pause
     goto menu
 )
